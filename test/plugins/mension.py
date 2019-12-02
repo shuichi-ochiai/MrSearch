222766ae9b2397471fec36d89c29f9fe06d58f07
miyazak <tmiyat06@gmail.com>
2019-12-02 19:53:05 +0900

    検索コマンド用の関数を追加

--- test/plugins/mension.py
+++ test/plugins/mension.py
@@ -6,3 +6,16 @@ def help_command(message):
     message.reply("""助けるよ⭐️
     検索 - @mr_search メシ [フリーワード(単語ごとにカンマ区切り)]
     """)
+
+
+@respond_to('メシ')
+def search_command(message):
+    search_word = message.body['text']
+
+    # TODO: search_wordは、'メシ'も含まれているので、その対策が必要
+
+    # TODO: ぐるなびAPIを呼ぶ
+
+    # TODO: responseを整形してreply
+
+    print(search_word)
